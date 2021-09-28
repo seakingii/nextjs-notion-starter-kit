168b2ea74364096b30b262a73aa29d4a8e6683b7
seakingii <seaking2@qq.com>
2021-09-28 23:18:08 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'b0bb13c9edc640a1af6183a50381d0e1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Hwj Blog',
+  domain: 'blog.hwj.im',
+  author: 'Seakingii',
 
   // open graph metadata (optional)
   description: 'Example site description',
